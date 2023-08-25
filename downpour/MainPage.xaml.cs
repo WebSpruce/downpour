9702c211a277ea2dca9c46df355f3d8bf1a3445e
WebSpruce <kacperswi12363@gmail.com>
2023-08-25 10:45:14 +0200

    Init

---
+++ downpour/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace downpour;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+
---
+++ downpour/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace downpour;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Liteon.otf", "LiteOn");
+				fonts.AddFont("Liteon-Black.otf", "LiteOnBlack");
+				fonts.AddFont("Liteon-Bold.otf", "LiteOnBold");
+				fonts.AddFont("Liteon-Light.otf", "LiteOnLight");
+				fonts.AddFont("Liteon-Medium.otf", "LiteOnMedium");
+				fonts.AddFont("Liteon-Thin.otf", "LiteOnThin");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
